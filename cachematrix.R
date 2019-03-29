64d505771b918b93d520fa5f9754c8b3a8615a4a
sparimoo95 <31860708+sparimoo95@users.noreply.github.com>
2019-03-28 20:46:23 -0400

    invertible matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,17 @@
 ## functions do
 
 ## Write a short comment describing this function
+# this function creates a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  as.matrix(x)
 }
 
 
 ## Write a short comment describing this function
+# this function computes the inverse of the special matrix object created above
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+  solve(x)
 }
